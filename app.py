caa71e09b88ffac6aa914bb3c8a7c57e4753a69e
ryandomeara06 <ryan.d.omeara06@gmail.com>
2026-04-29 17:40:49 -0500

    Rename app title to 'MarketView Analytics'

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 
 st.set_page_config(page_title = "Final Project", layout="wide")
 
-st.title("Data Analytics App")
+st.title("MarketView Analytics")
 
 def display_stock_indicator():
     st.write("Extract stock market prices from yahoo finance using ticker")
